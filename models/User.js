969ecedfcaddddd16ea4b58370fbd38b2dc82224
Sukhyun Baik <neonagx@gmail.com>
2017-02-21 11:41:22 -0800

    create user model schema on user.js

---
+++ models/User.js
@@ -0,0 +1,17 @@
+'use strict'
+const mongoose = require('mongoose')
+
+const User = new mongoose.Schema({
+  email     : { type: String, required: true, unique: true },
+  password  : { type: String, required: true },
+  firstName : { type: String },
+  lastName  : { type: String },
+  phone     : { type: String },
+  image     : Image,
+  location  : { type: String },
+  lessons   : { type: Number },
+  admin     : { type: Boolean, default: false},
+  forgotPw  : { type: String }
+})
+
+module.exports = mongoose.model('User', User)
